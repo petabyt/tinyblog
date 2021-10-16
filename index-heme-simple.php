451c9652e4cc012f7c1ae983773fcf86639b3a8d
夏目贵志 <6@9i3.cn>
2021-10-16 03:01:31 +0000

    Update index-heme-simple.php

--- index-heme-simple.php
+++ index-heme-simple.php
@@ -3,7 +3,7 @@
     <head>
 
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=0.5">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=2.0, user-scalable=yes" />
         <link rel="shortcut icon" href="https://petabyte.heb12.com/favicon.ico">
         <title>My Blog</title>
 
